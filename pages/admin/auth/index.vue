17e77687229626cfd7af5995602ad42c7ae4c54b
DESKTOP-IF0P0KI\linsy <yana_586@mail.ru>
2021-12-07 00:42:32 +0300

    add auth page

---
+++ pages/admin/auth/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="auth">
+    <div class="container">
+      <form @submit.prevent="onSubmit" class="auth__form">
+        <AppInput type="email" v-model="user.email"> Login: </AppInput>
+        <AppInput type="password" v-model="user.password"> Password: </AppInput>
+
+        <div class="controls">
+          <AppButton>Login</AppButton>
+        </div>
+
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      user: {
+        email: '',
+        password: '',
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      this.user.email = ''
+      this.user.password = ''
+    }
+  },
+};
+</script>
+
+<style lang="scss">
+.auth {
+  margin: 0 auto;
+  max-width: 400px;
+  text-align: center;
+
+  &__form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 60vh;
+  }
+}
+</style>
